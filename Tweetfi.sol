9364f84eac2a3d3b555ec2a8d49b9c03b9ae6aec
Tweetfi <121546777+Tweetfi@users.noreply.github.com>
2023-01-09 16:45:48 +0800

    Update Tweetfi.sol
    update  _transfer method

--- Tweetfi.sol
+++ Tweetfi.sol
@@ -52,7 +52,18 @@ abstract contract Ownable is Context {
         emit OwnershipTransferred(oldOwner, newOwner);
     }
 }
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
+ * now has built in overflow checking.
+ */
 library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
     function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
     unchecked {
         uint256 c = a + b;
@@ -60,72 +71,187 @@ library SafeMath {
         return (true, c);
     }
     }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
     function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
     unchecked {
         if (b > a) return (false, 0);
         return (true, a - b);
     }
     }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
     function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
     unchecked {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
         if (a == 0) return (true, 0);
         uint256 c = a * b;
         if (c / a != b) return (false, 0);
         return (true, c);
     }
     }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
     function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
     unchecked {
         if (b == 0) return (false, 0);
         return (true, a / b);
     }
     }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
     function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
     unchecked {
         if (b == 0) return (false, 0);
         return (true, a % b);
     }
     }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         return a + b;
     }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         return a - b;
     }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         return a * b;
     }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return a / b;
     }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
         return a % b;
     }
-    function sub(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
     unchecked {
         require(b <= a, errorMessage);
         return a - b;
     }
     }
-    function div(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
     unchecked {
         require(b > 0, errorMessage);
         return a / b;
     }
     }
-    function mod(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
     unchecked {
         require(b > 0, errorMessage);
         return a % b;
@@ -137,13 +263,10 @@ contract TweetFi is Context, IBEP20, Ownable {
     using SafeMath for uint256;
     mapping (address => uint256) private _balances;
     mapping (address => mapping (address => uint256)) private _allowances;
-    mapping(address => bool) private presale_ads;
     uint256 private _totalSupply;
     uint8 private _decimals;
     string private _symbol;
     string private _name;
-    address public _liquidityAddress;
-    uint256 public _liquidityFee = 20;
     constructor() {
         _name = "TweetFi";
         _symbol = "TWF";
@@ -151,9 +274,6 @@ contract TweetFi is Context, IBEP20, Ownable {
         _totalSupply = 100000000 * (10**uint256(_decimals));
         _balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
-    }
-    function setLiquidityAddress(address liquidityAddress) external onlyOwner {
-        _liquidityAddress = liquidityAddress;
     }
     function getOwner() external view override returns (address) {
         return owner();
@@ -172,10 +292,6 @@ contract TweetFi is Context, IBEP20, Ownable {
     }
     function balanceOf(address account) external view override returns (uint256) {
         return _balances[account];
-    }
-    function excludeFromFee(address ad) public onlyOwner {
-        require(!presale_ads[ad], "This pair is already excluded");
-        presale_ads[ad] = true;
     }
     function transfer(address recipient, uint256 amount) external override returns (bool) {
         _transfer(_msgSender(), recipient, amount);
@@ -205,16 +321,8 @@ contract TweetFi is Context, IBEP20, Ownable {
         require(sender != address(0), "BEP20: transfer from the zero address");
         require(recipient != address(0), "BEP20: transfer to the zero address");
         _balances[sender] = _balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
-        uint256 liquidityAmount = amount.mul(_liquidityFee).div(10**3);
-        if(!presale_ads[sender] && liquidityAmount > 0 &&  _liquidityAddress != address(0)){
-            _balances[_liquidityAddress] = _balances[_liquidityAddress].add(liquidityAmount);
-            emit Transfer(sender, _liquidityAddress, liquidityAmount);
-        }else{
-            liquidityAmount=0;
-        }
-        uint256 residualAmount = amount.sub(liquidityAmount);
-        _balances[recipient] = _balances[recipient].add(residualAmount);
-        emit Transfer(sender, recipient, residualAmount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
     }
     function _approve(address owner, address spender, uint256 amount) internal {
         require(owner != address(0), "BEP20: approve from the zero address");
